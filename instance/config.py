7f52ddb05f51d5cb6f7826d8e5047eac2e94af45
gitvinhub <41100948+gitvinhub@users.noreply.github.com>
2019-07-16 18:31:47 +0530

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -5,3 +5,4 @@ DB_HOST = os.environ.get("mysql_host")
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
 #SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@70.0.0.4/dreamteam_db'
 SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@' + DB_HOST + '/dreamteam_db'
+print SQLALCHEMY_DATABASE_URI
